54df10bee83bfab9b70b4dc197d2e78359eb46bf
Samuel Fisher <fisher_samuel_adam01@student.smc.edu>
2025-09-29 12:47:56 -0700

    svelte.config changes

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,16 @@ const config = {
   preprocess: [preprocess({ postcss: true })],
 
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      pages: "build",
+      assets: "build",
+      fallback: null,
+      precompress: false,
+      strict: true
+    }),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '' : ''
+    }
   },
 };
 
